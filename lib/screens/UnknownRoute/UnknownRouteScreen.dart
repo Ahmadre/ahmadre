3920f31b1f34ddb5f99fc8aaff6327a790e9199e
Ahmadre <rebar.ahmad@telexiom.de>
2020-05-17 22:50:00 +0200

    Setup 404 page

--- lib/screens/UnknownRoute/UnknownRouteScreen.dart
+++ lib/screens/UnknownRoute/UnknownRouteScreen.dart
@@ -5,6 +5,24 @@ class UnknownRouteScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              '404',
+              textScaleFactor: 10,
+            ),
+            Text(
+              'Lost in Flutter!',
+              textScaleFactor: 3,
+            ),
+            Text('Sorry the page you\'re looking for isn\'t here 😮'),
+          ],
+        ),
+      ),
+    );
   }
 }
